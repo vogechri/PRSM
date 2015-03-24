5e94020681309549e57a6b8f2ca33c60ed0bbfa9
cloude123 <vogel@geod.baug.ethz.ch>
2015-03-24 19:24:41 +0100

    test case:

--- solvePWRSMulti/io/loadKittiFlow.m
+++ solvePWRSMulti/io/loadKittiFlow.m
@@ -3,7 +3,7 @@ function [cam, ref, imageName, flow2DGt, flow2DGt_noc] = ...
 
 % loading the gt or some other stuff - no need for that only for painting
 
-testing = p.testing;
+testing   = p.testing;
 ImgL      = cell(1,2*p.frames+2);
 ImgR      = cell(1,2*p.frames+2);
 
@@ -11,26 +11,20 @@ strNumber0 = sprintf( '%06d_%02d', nr, p.subImg );
 strNumber1 = sprintf( '%06d_%02d', nr, p.subImg+1 );
 imageName  = sprintf('Kitti_%s', strNumber0);
 
-localFolder = strFolder;
 if ~testing
-
-sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumber0 )
-
-  ImgL{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumber0 )))/255;
-  ImgR{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumber0 )))/255;
-  ImgL{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumber1 )))/255;
-  ImgR{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumber1 )))/255;
-  calibFile = sprintf('%s/%s/%06d.txt', localFolder, '/data_stereo_flow/training/calib', nr );
+  localFolder = sprintf('%s/',strFolder, 'data_stereo_flow/training');
 else
-  ImgL{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/testing/image_0', strNumber0 )))/255;
-  ImgR{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/testing/image_1', strNumber0 )))/255;
-  ImgL{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/testing/image_0', strNumber1 )))/255;
-  ImgR{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/testing/image_1', strNumber1 )))/255;
-  calibFile = sprintf('%s/%s/%06d.txt', localFolder, '/data_stereo_flow/testing/calib', nr );
+  localFolder = sprintf('%s/',strFolder, 'data_stereo_flow/testing');
 end
 
-data_supp = loadCalibrationKITTI_new( calibFile );
-data_supp.I = ImgR;
+  ImgL{1} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumber0 )))/255;
+  ImgR{1} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumber0 )))/255;
+  ImgL{2} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumber1 )))/255;
+  ImgR{2} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumber1 )))/255;
+  calibFile = sprintf('%s/%s/%06d.txt', localFolder, '/calib', nr );
+
+  data_supp = loadCalibrationKITTI_new( calibFile );
+  data_supp.I = ImgR;
 
 gt_Name = sprintf('%06d_10', nr);
 
@@ -61,24 +55,24 @@ flow2DGt     = cat (3, DispImg, flowImg);
 if p.subImg>0
   strNumberprev = sprintf( '%06d_%02d', nr, p.subImg-1 );
   try
-    ImgOld = cell(1,2*p.frames+2);
-    ImgOld{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumberprev )))/255;
-    ImgOld{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumberprev )))/255;
-    cam.Iold = ImgOld;
+  ImgOld = cell(1,2*p.frames+2);
+  ImgOld{1} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumberprev )))/255;
+  ImgOld{2} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumberprev )))/255;
+  cam.Iold = ImgOld;
   catch
-    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumberprev ) );
-    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumberprev ) );
+    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumberprev ) );
+    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumberprev ) );
   end
 end
 if p.subImg<19
   strNumberprev = sprintf( '%06d_%02d', nr, p.subImg+2 );
   try
   Imgnew = cell(1,2*p.frames+2);
-  Imgnew{1} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumberprev )))/255;
-  Imgnew{2} = double(imread(sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumberprev )))/255;
+  Imgnew{1} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumberprev )))/255;
+  Imgnew{2} = double(pngStuff(sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumberprev )))/255;
   cam.Inew = Imgnew;
   catch
-    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_0', strNumberprev ) );
-    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/data_stereo_flow/training/image_1', strNumberprev ) );
-  end  
+    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/image_0', strNumberprev ) );
+    fprintf('Failed to load images: %s', sprintf('%s/%s/%s.png', localFolder, '/image_1', strNumberprev ) );
+  end
 end
